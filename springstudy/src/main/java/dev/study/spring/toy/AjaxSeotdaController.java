d2c8b79e74b6eaf2d450817c927b51cf90b3204f
Auden <sasameyuki03@naver.com>
2022-12-10 21:17:29 +0900

    2022-12-10 섯다게임으로 ajax 비동기통신 이해하기 완료.

--- springstudy/src/main/java/dev/study/spring/toy/AjaxSeotdaController.java
+++ springstudy/src/main/java/dev/study/spring/toy/AjaxSeotdaController.java
@@ -14,26 +14,74 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxSeotdaController {
-	
+	// 통신하는 값은 전역변수로 존재하면 안된다. 만약 새로운 사람이 들어왔을때 초기화된 값을 보여줘야 하는데 그럴수가 없기 때문.
+	// 기본적으로 스프링 빈에서 싱글톤으로 생성되기 때문에 전역변수로 사용하면 전혀 다른 결과가 나올수도 있다.
+	// 혼자쓸거면 상관없는데 2명이상이서 쓰게되면 값이 어떻게 될지 모른다.
+
 	@RequestMapping(value = "/firstajax", method = RequestMethod.GET)
-	public String connectAjax() {
+	public String connectAjax(Model model, CardDeck carddeck, Card card) {
 		// 여기 있는 메소드로 맵핑된 url로 jsp 최초진입이 시작된다, 그러므로 GET방식으로 선언해 주어야 한다.
-		// 최초진입은 url주소를 브라우저에 "직접 기입하여" 접속한다. url에 표시되서 전달되는건 뭐다? 대깨get이란거다.
-		return "toyprj/ajaxjsphouse";
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/ajaxjspdobak", method = RequestMethod.POST)
-	public Map<String, Object> getDeck(@RequestParam Map<String, Object> param,
-				@RequestParam("first") String firLi, @RequestParam("second") String secLi, Model model) {
-		
+		// 최초진입은 url주소를 브라우저에 "직접 기입하여" 접속한다.
+
 		// 1번째 플레이어의 카드를 담을 리스트 생성
 		List<Card> firstCardList = new ArrayList<>();
+
+		// 카드 덱 객체 생성.
+		// 섞음
+		carddeck.shuffle();
+
+		// 1번 플레이어의 1번째카드, 2번째카드 얻는 변수.
+		String firstHanded = carddeck.getCards().get(0).getCardNum();
+		String secondHanded = carddeck.getCards().get(2).getCardNum();
+		// 승부 판별용 점수만 따로 계산
+		int firstHandedNum = carddeck.getCards().get(0).getNum();
+		int secondHandedNum = carddeck.getCards().get(2).getNum();
+		// 1번 플레이어 총 합 점수
+		int firstPlayerTotalScore = firstHandedNum + secondHandedNum;
+
+		// ----------------------------------------------------------
+
+		// 어떤 카드를 받았는지를 넣어주고 카드 리스트에 추가.
+		// 1번 플레이어의 1번째 카드
+		card.setCardNum(firstHanded);
+		// 카드 추가
+		firstCardList.add(card);
+
+		// 두번째 카드
+		card = new Card();
+
+		// 마찬가지
+		card.setCardNum(secondHanded);
+		firstCardList.add(card);
+
+//		Map<String, Object> inputPageCardList = new HashMap<>();
+//		inputPageCardList.put("inputPageCardList", firstCardList);
+//
+//		model.addAttribute("inputPageCardList", inputPageCardList);
+		model.addAttribute("firstCard", firstHanded);
+		model.addAttribute("secondCard", secondHanded);
+		model.addAttribute("totalScore", firstPlayerTotalScore);
+
+		// System.out.println("1번 플레이어 " + inputPageCardList.toString());
+
+		// 일단 model값부터 보내봄. 전달 됨.
+//		model.addAttribute("num1", 1);
+//		model.addAttribute("num2", 2);
+		return "toyprj/ajaxjsphouse";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/ajaxjspdobak", method = RequestMethod.POST)
+	public Map<String, Object> getDeck(@RequestParam Map<String, Object> param, @RequestParam("first") String first,
+			@RequestParam("second") String second, CardDeck carddeck, Card card) {
+
+		// 1번째 플레이어의 카드를 담을 리스트 생성
+//		List<Card> firstCardList = new ArrayList<>();
 		// 2번째 플레이어의 카드를 담을 리스트 생성
 		List<Card> secondCardList = new ArrayList<>();
-		
+
 		// 1번 플레이어 카드 객체 생성
-		Card card = new Card();
+//		Card card = new Card();
 		// 2번 플레이어 카드 객체 생성
 		Card card2 = new Card();
 		// 결과를 저장하고 출력할 문자열 생성
@@ -42,10 +90,10 @@ public class AjaxSeotdaController {
 		String firstPlayerDeckLevel = "";
 		String secondPlayerDeckLevel = "";
 		// 매직넘버 방지용 광패 변수, 광은 하나밖에 없는 패 이므로 final로 설정
-		int FIRST_LIGHT_NUM=11;
-		int THIRD_LIGHT_NUM=13;
-		int EIGHTH_LIGHT_NUM=18;
-		
+		int FIRST_LIGHT_NUM = 11;
+		int THIRD_LIGHT_NUM = 13;
+		int EIGHTH_LIGHT_NUM = 18;
+
 		// 작은 패를 첫번째 카드로 바꿔주기 위한 임시변수
 		int temp1 = 0; // 1번 플레이어
 		int temp2 = 0; // 2번 플레이어.
@@ -54,32 +102,77 @@ public class AjaxSeotdaController {
 		int secondPlayerLevel = 0;
 
 		// 카드 덱 객체 생성.
-		CardDeck cd = new CardDeck();
+//		CardDeck cd = new CardDeck();
 		// 섞음
-		cd.shuffle();
-
-		// 1번 플레이어의 1번째카드, 2번째카드 얻는 변수.
-		String firstHanded = cd.getCards().get(0).getCardNum();
-		String secondHanded = cd.getCards().get(2).getCardNum();
+		carddeck.shuffle();
 		
+		//-----------------------------------------------------------------------------
+		// 1번 플레이어의 1번째카드, 2번째카드 얻는 변수.
+		// 문자열로 넘어 간뒤 한번 더 문자열로 받아가지고 더블쿼테이션이 겹친다... ""1"" 이런식으로 나와서 numberformatException이 뜬다.
+		// 중복문자열 제거로 없애줘야한다.
+		String firstHanded = "";
+		for (int i = 1; i < first.length()-1; i++) {
+			if (!firstHanded.contains(String.valueOf(first.charAt(i)))) {
+				firstHanded+=String.valueOf(first.charAt(i));
+			}
+		}
+		
+		String secondHanded = "";
+		for (int i = 1; i < second.length()-1; i++) {
+			if (!secondHanded.contains(String.valueOf(second.charAt(i)))) {
+				secondHanded+=String.valueOf(second.charAt(i));
+			}
+		}
+		// 무조건 테스트
+		System.out.println("넘어오고 변환한 first 값 : " + firstHanded + "");
+		System.out.println("넘어오고 변환한 second 값 : " + secondHanded + "");
+		//-----------------------------------------------------------------------------
 		// 승부 판별용 점수만 따로 계산
-		int firstHandedNum = cd.getCards().get(0).getNum();
-		int secondHandedNum = cd.getCards().get(2).getNum();
+		int firstHandedNum;
+		int secondHandedNum;
+
+		// 페이지 진입시에 받은 카드를 이쪽 컨트롤러로 넘겨받았다, 그 값은 문자열이며 광도 섞여있는데 "광"이 섞여있는 경우
+		// parseint로 타입변환을 하지 못하여 500에러가 난다. 좀 길지만 if문으로 레벨디자인 했던 숫자로 강제변환 해주어야 한다.
+		// 여기서 다시한번 느꼈다. 승부 판정을 레벨디자인 변수를 만들어둔건 진짜 이세돌의 백78수였다.
+
+		if (firstHanded.equals("1광")) {
+			firstHandedNum = FIRST_LIGHT_NUM;
+		} else if (firstHanded.equals("3광")) {
+			firstHandedNum = THIRD_LIGHT_NUM;
+		} else if (firstHanded.equals("8광")) {
+			firstHandedNum = EIGHTH_LIGHT_NUM;
+		} else {
+			firstHandedNum = Integer.parseInt(firstHanded);
+		}
+
+		if (secondHanded.equals("1광")) {
+			secondHandedNum = FIRST_LIGHT_NUM;
+		} else if (secondHanded.equals("3광")) {
+			secondHandedNum = THIRD_LIGHT_NUM;
+		} else if (secondHanded.equals("8광")) {
+			secondHandedNum = EIGHTH_LIGHT_NUM;
+		} else {
+			secondHandedNum = Integer.parseInt(secondHanded);
+		}
+
 		// 1번 플레이어 총 합 점수
 		int firstPlayerTotalScore = firstHandedNum + secondHandedNum;
 
 		// 2번 플레이어의 1번째 카드, 2번째 카드 얻는 변수.
-		String firstHandedSecondPlayer = cd.getCards().get(1).getCardNum();
-		String secondHandedSecondPlayer = cd.getCards().get(3).getCardNum();
+		String firstHandedSecondPlayer = carddeck.getCards().get(1).getCardNum();
+		String secondHandedSecondPlayer = carddeck.getCards().get(3).getCardNum();
 		// 2번 플레이어 승부 판별용 점수
-		int secFirstHandedNum = cd.getCards().get(1).getNum();
-		int secSecondHandedNum = cd.getCards().get(3).getNum();
+		int secFirstHandedNum = carddeck.getCards().get(1).getNum();
+		int secSecondHandedNum = carddeck.getCards().get(3).getNum();
 		// 2번 플레이어 총 합 점수
 		int secondPlayerTotalScore = secFirstHandedNum + secSecondHandedNum;
-		
+
+		// ----------------------------------------------------------
+
 		// 승부 가르기
 		// 승부는 까다롭다. 이긴 플레이어에 따라서 점수를 계산해야한다.
-		// [1] 광땡을 먼저 계산. 13, 18, 38이 있고 38광땡은 최대 레벨이므로 모든 패를 이긴다. 13광땡과 18광땡은 확률이 동일하므로 동일레벨로본다.
+		// [1] 광땡을 먼저 계산. 13, 18, 38이 있고 38광땡은 최대 레벨이므로 모든 패를 이긴다. 13광땡과 18광땡은 확률이 동일하므로
+		// 동일레벨로본다.
 		// [2] 13, 18 광땡은 38광땡만 못이긴다.
 		// [3] 땡은 순서대로 10 9 8...1 땡으로 큰 수부터 서열순으로 정렬된다. 모든 땡은 모든 광땡을 이기지 못한다.
 		// [4] 모든 끗은 모든 땡 종류를 이기지 못하고 끗 내에서만 정리가된다.
@@ -88,8 +181,8 @@ public class AjaxSeotdaController {
 		// [7] 무승부여도 4끗과 2땡은 엄연히 다르다. 무승부 마저도 조건검사를 걸어야한다.
 		// -- 위의 조건들을 만족하기 위해선 등급을 판별하는 객체를 따로 만들어서 검사를 해 줄 경우 편리해진다. 하나 더 만들자.
 		// [8] 1번카드와 2번카드를 각각 개별 객체로 나누면 까다로워진다. 손에 들어온 패는 무조건 순서를 지킬 필요는없다. 만약
-		// 		작은 패를 앞쪽으로 넣어주면 조건이 매우 편해진다.
-		
+		// 작은 패를 앞쪽으로 넣어주면 조건이 매우 편해진다.
+
 		// [8] 작은 패 먼저 앞으로 옮기기.
 		// 1번 플레이어
 		if (firstHandedNum > secondHandedNum) {
@@ -103,170 +196,161 @@ public class AjaxSeotdaController {
 			secFirstHandedNum = secSecondHandedNum;
 			secSecondHandedNum = temp2;
 		}
-		
+
 		// [1] 광땡 계산하기
 		// 1번 플레이어 레벨검사
-		if (firstHandedNum==THIRD_LIGHT_NUM && secondHandedNum==EIGHTH_LIGHT_NUM) {
+		if (firstHandedNum == THIRD_LIGHT_NUM && secondHandedNum == EIGHTH_LIGHT_NUM) {
 			firstPlayerDeckLevel = "38광땡";
 			firstPlayerLevel = 100;
-		} else if (firstHandedNum==FIRST_LIGHT_NUM && secondHandedNum==THIRD_LIGHT_NUM) {
+		} else if (firstHandedNum == FIRST_LIGHT_NUM && secondHandedNum == THIRD_LIGHT_NUM) {
 			firstPlayerDeckLevel = "13광땡";
 			firstPlayerLevel = 99;
-		} else if (firstHandedNum==FIRST_LIGHT_NUM && secondHandedNum==EIGHTH_LIGHT_NUM) {
+		} else if (firstHandedNum == FIRST_LIGHT_NUM && secondHandedNum == EIGHTH_LIGHT_NUM) {
 			firstPlayerDeckLevel = "18광땡";
 			firstPlayerLevel = 99;
 		}
 		// 2번 플레이어 레벨검사
-		if (secFirstHandedNum==THIRD_LIGHT_NUM && secSecondHandedNum==EIGHTH_LIGHT_NUM) {
+		if (secFirstHandedNum == THIRD_LIGHT_NUM && secSecondHandedNum == EIGHTH_LIGHT_NUM) {
 			secondPlayerDeckLevel = "38광땡";
 			secondPlayerLevel = 100;
-		} else if (secFirstHandedNum==FIRST_LIGHT_NUM && secSecondHandedNum==THIRD_LIGHT_NUM) {
+		} else if (secFirstHandedNum == FIRST_LIGHT_NUM && secSecondHandedNum == THIRD_LIGHT_NUM) {
 			secondPlayerDeckLevel = "13광땡";
 			secondPlayerLevel = 99;
-		} else if (secFirstHandedNum==FIRST_LIGHT_NUM && secSecondHandedNum==EIGHTH_LIGHT_NUM) {
+		} else if (secFirstHandedNum == FIRST_LIGHT_NUM && secSecondHandedNum == EIGHTH_LIGHT_NUM) {
 			secondPlayerDeckLevel = "18광땡";
 			secondPlayerLevel = 99;
 		}
 		// [2] 땡계산
 		// 1번 플레이어
-		if (firstHandedNum==10 && secondHandedNum==10) {
+		if (firstHandedNum == 10 && secondHandedNum == 10) {
 			firstPlayerDeckLevel = "장땡";
 			firstPlayerLevel = 50;
-		} else if(firstHandedNum==9 && secondHandedNum==9) {
+		} else if (firstHandedNum == 9 && secondHandedNum == 9) {
 			firstPlayerDeckLevel = "구땡";
 			firstPlayerLevel = 49;
-		} else if(firstHandedNum==8 && secondHandedNum==EIGHTH_LIGHT_NUM) {
+		} else if (firstHandedNum == 8 && secondHandedNum == EIGHTH_LIGHT_NUM) {
 			firstPlayerDeckLevel = "팔땡";
 			firstPlayerLevel = 48;
-		} else if(firstHandedNum==7 && secondHandedNum==7) {
+		} else if (firstHandedNum == 7 && secondHandedNum == 7) {
 			firstPlayerDeckLevel = "칠땡";
 			firstPlayerLevel = 47;
-		} else if(firstHandedNum==6 && secondHandedNum==6) {
+		} else if (firstHandedNum == 6 && secondHandedNum == 6) {
 			firstPlayerDeckLevel = "육땡";
 			firstPlayerLevel = 46;
-		} else if(firstHandedNum==5 && secondHandedNum==5) {
+		} else if (firstHandedNum == 5 && secondHandedNum == 5) {
 			firstPlayerDeckLevel = "오땡";
 			firstPlayerLevel = 45;
-		} else if(firstHandedNum==4 && secondHandedNum==4) {
+		} else if (firstHandedNum == 4 && secondHandedNum == 4) {
 			firstPlayerDeckLevel = "사땡";
 			firstPlayerLevel = 44;
-		} else if(firstHandedNum==3 && secondHandedNum==THIRD_LIGHT_NUM) {
+		} else if (firstHandedNum == 3 && secondHandedNum == THIRD_LIGHT_NUM) {
 			firstPlayerDeckLevel = "삼땡";
 			firstPlayerLevel = 43;
-		} else if(firstHandedNum==2 && secondHandedNum==2) {
+		} else if (firstHandedNum == 2 && secondHandedNum == 2) {
 			firstPlayerDeckLevel = "이땡";
 			firstPlayerLevel = 42;
-		} else if(firstHandedNum==1 && secondHandedNum==FIRST_LIGHT_NUM) {
+		} else if (firstHandedNum == 1 && secondHandedNum == FIRST_LIGHT_NUM) {
 			firstPlayerDeckLevel = "일땡";
 			firstPlayerLevel = 41;
 		}
-		
+
 		// 2번 플레이어
-		if (secFirstHandedNum==10 && secSecondHandedNum==10) {
+		if (secFirstHandedNum == 10 && secSecondHandedNum == 10) {
 			secondPlayerDeckLevel = "장땡";
 			secondPlayerLevel = 50;
-		} else if(secFirstHandedNum==9 && secSecondHandedNum==9) {
+		} else if (secFirstHandedNum == 9 && secSecondHandedNum == 9) {
 			secondPlayerDeckLevel = "구땡";
 			secondPlayerLevel = 49;
-		} else if(secFirstHandedNum==8 && secSecondHandedNum==EIGHTH_LIGHT_NUM) {
+		} else if (secFirstHandedNum == 8 && secSecondHandedNum == EIGHTH_LIGHT_NUM) {
 			secondPlayerDeckLevel = "팔땡";
 			secondPlayerLevel = 48;
-		} else if(secFirstHandedNum==7 && secSecondHandedNum==7) {
+		} else if (secFirstHandedNum == 7 && secSecondHandedNum == 7) {
 			secondPlayerDeckLevel = "칠땡";
 			secondPlayerLevel = 47;
-		} else if(secFirstHandedNum==6 && secSecondHandedNum==6) {
+		} else if (secFirstHandedNum == 6 && secSecondHandedNum == 6) {
 			secondPlayerDeckLevel = "육땡";
 			secondPlayerLevel = 46;
-		} else if(secFirstHandedNum==5 && secSecondHandedNum==5) {
+		} else if (secFirstHandedNum == 5 && secSecondHandedNum == 5) {
 			secondPlayerDeckLevel = "오땡";
 			secondPlayerLevel = 45;
-		} else if(secFirstHandedNum==4 && secSecondHandedNum==4) {
+		} else if (secFirstHandedNum == 4 && secSecondHandedNum == 4) {
 			secondPlayerDeckLevel = "사땡";
 			secondPlayerLevel = 44;
-		} else if(secFirstHandedNum==3 && secSecondHandedNum==THIRD_LIGHT_NUM) {
+		} else if (secFirstHandedNum == 3 && secSecondHandedNum == THIRD_LIGHT_NUM) {
 			secondPlayerDeckLevel = "삼땡";
 			secondPlayerLevel = 43;
-		} else if(secFirstHandedNum==2 && secSecondHandedNum==2) {
+		} else if (secFirstHandedNum == 2 && secSecondHandedNum == 2) {
 			secondPlayerDeckLevel = "이땡";
 			secondPlayerLevel = 42;
-		} else if(secFirstHandedNum==1 && secSecondHandedNum==FIRST_LIGHT_NUM) {
+		} else if (secFirstHandedNum == 1 && secSecondHandedNum == FIRST_LIGHT_NUM) {
 			secondPlayerDeckLevel = "일땡";
 			secondPlayerLevel = 41;
 		}
-		// 끗 계산하기
-		// 1번 2번 동시 가능
-//		if (firstPlayerTotalScore%10 > secondPlayerTotalScore%10) {
-//			result = "1번 플레이어가 " + firstPlayerTotalScore%10 + " 끗으로 이겼습니다!";
-//		} else {
-//			result = "2번 플레이어가 " + secondPlayerTotalScore%10 + " 끗으로 이겼습니다!";
-//		}
-//		
+
 		// 무승부
 		if ((firstPlayerTotalScore % 10) == (secondPlayerTotalScore % 10)) {
 			result = "무승부네요!";
 		}
-		
+
 		// 레벨과 총합계점수로 최종판별 (레벨 : 땡판별 / 합계점수 : 끗판별)
 		// 1번 플레이어
 		if (firstPlayerLevel > secondPlayerLevel) {
 			result = "1번 플레이어가 " + firstPlayerDeckLevel + " 으로 이겼습니다!";
-		} else if (firstPlayerTotalScore%10 > secondPlayerTotalScore%10) {
-			result = "1번 플레이어가 " + firstPlayerTotalScore%10 + " 끗으로 이겼습니다!";
+		} else if (firstPlayerTotalScore % 10 > secondPlayerTotalScore % 10) {
+			result = "1번 플레이어가 " + firstPlayerTotalScore % 10 + " 끗으로 이겼습니다!";
 		}
 		// 2번 플레이어
 		if (firstPlayerLevel < secondPlayerLevel) {
 			result = "2번 플레이어가 " + secondPlayerDeckLevel + " 으로 이겼습니다!";
-		} else if (firstPlayerTotalScore%10 < secondPlayerTotalScore%10) {
-			result = "2번 플레이어가 " + secondPlayerTotalScore%10 + " 끗으로 이겼습니다!";
+		} else if (firstPlayerTotalScore % 10 < secondPlayerTotalScore % 10) {
+			result = "2번 플레이어가 " + secondPlayerTotalScore % 10 + " 끗으로 이겼습니다!";
 		}
-		
+
 		// 승부 판별 종료.
 		// [1] 레벨 변수 하나 만들어서 관리하길 너무잘함.
 		// 단순 숫자만 놓고 보는 블랙잭과는 달리 섯다는 족보란게 존재하여 단순 숫자가 낮아도 상대방을 이길 수 있는 패가 매우 많음.
 		// 이에 착안하여 level을 판별하는 변수를 추가. => 박수짝짝
-		
+
 		// [2] 1번카드 2번카드는 프로그래밍일 뿐 현실에선 순서가 중요하지 않다는 점에 착안하여 생각을 바꿔봄.
 		// 바꾼 생각 -> 숫자가 작은걸 앞쪽에 몰아넣자. 어차피 결과점수는 똑같다. => 매우 잘한일, 코드 단축 및 생산성이 극대화됨.
 
 		// 어떤 카드를 받았는지를 넣어주고 카드 리스트에 추가.
 		// 1번 플레이어의 1번째 카드
-		card.setCardNum(firstHanded);
+//		card.setCardNum(firstHanded);
 		// 2번 플레이어의 1번째 카드
 		card2.setCardNum(firstHandedSecondPlayer);
 		// 각각 카드 추가
-		firstCardList.add(card);
+//		firstCardList.add(card);
 		secondCardList.add(card2);
 
 		// 두번째 카드
-		card = new Card();
+//		card = new Card();
 		card2 = new Card();
-		
+
 		// 마찬가지
-		card.setCardNum(secondHanded);
+//		card.setCardNum(secondHanded);
 		card2.setCardNum(secondHandedSecondPlayer);
-		firstCardList.add(card);
+//		firstCardList.add(card);
 		secondCardList.add(card2);
 
 		Map<String, Object> cardList = new HashMap<>();
-		cardList.put("cardList", firstCardList);
+//		cardList.put("cardList", firstCardList);
 		cardList.put("cardList2", secondCardList);
 		cardList.put("result", result);
 		cardList.put("connect", "success");
 		cardList.put("connect2", "success2");
-		model.addAttribute("cardList", firstCardList);
-		
-		//ajax에서 들어오는 값을 알기 위한 테스트코드
-//		String list = req.getParameter("list");
-//		System.out.println("list : " + list);
-		System.out.println("first : " + firLi + "\t");
-//		System.out.println(req.("list"));
-		
-//		String list2 = req.getParameter("list2");
-//		System.out.println("list2 : " + list2);
-		System.out.println("second : " + secLi + "\t");
-//		System.out.println(req.getParameter("list2"));
-//		System.out.println(rsp.getParameterMap());
-		
+
+		// ------------------------------------------------------------------------
+
+		// ajax에서 들어오는 값을 알기 위한 테스트코드
+		System.out.println("파람값1 : " + param.get("first"));
+		System.out.println("파람값2 : " + param.get("second"));
+		System.out.println("@RequestParam(\"first\")의 값" + first);
+		System.out.println("@RequestParam(\"second\")의 값" + second);
+		System.out.println("ajax에서 넘어온 param의 키" + param.keySet());
+		System.out.println("ajax에서 넘어온 param의 값" + param.values());
+//	
+
 		return cardList;
 	}
 
--- springstudy/src/main/webapp/WEB-INF/views/toyprj/ajaxjsphouse.jsp
+++ springstudy/src/main/webapp/WEB-INF/views/toyprj/ajaxjsphouse.jsp
@@ -33,7 +33,8 @@ thead {
 		<tbody>
 			<tr>
 				<td>
-					<div id="first"></div>
+					<div id="first">1번째 패 : ${firstCard}<br>
+					2번째 패 : ${secondCard}</div>
 				</td>
 				<td>
 					<div id="second">딜러의 패는 승부판정과 동시에 오픈됩니다.</div>
@@ -48,22 +49,23 @@ thead {
 
 </body>
 <script type="text/javascript">
+
 	// 1. 아래 변수 선언 위치도 제대로 모르고 선언함 ajax 코드 안에는 변수선언 하면 안됨.
 	// 2. ajax안에 error를 지 맘대로씀
 	// 3. 스크립트 에러나는데 개발자도구 1도안쳐다봄 (f12의 습관화)
 	// 4. success에서 가져온 data console.log로 찍으면 되는데 시도 조차 안함.
 	// (f12에서 브레이크포인트 사용이 가능하다 꼭 쓸것, 브레이크 포인트 쓰기 전 console.log() 겁나게 찍자 콘솔로그엔 답이있다..!)
-
-	// 컨트롤러에서 넘어온 변수를 저장할 값
-	// 자바스크립트 배열 선언방식.
 	
+	// 파라미터를 js객체화
+	let cardData1 = '${firstCard}';
+	let cardData2 = '${secondCard}';
 	
-	/* let list3 = [{'cardList': 'spade', 'cardList2':'clover'}]; */
-	let firstList = [];
-		let secondList = [];
+	console.log(cardData1);
+	console.log(cardData2);
+	// 객체화 테스트 출력 ok
+	
 	function openCard() {
-		
-		var list = [];
+		// 딜러의 카드를 담아서 넘겨버릴 배열변수 선언.
 		var list2 = [];
 		
 		$.ajax({
@@ -73,48 +75,53 @@ thead {
 			url : "/ajaxjspdobak",
 			data : {
 				// 여기서 데이터를 넘겨야함.
-				"first" : JSON.stringify(firstList),
-				"second" : JSON.stringify(secondList)
+				'first' : JSON.stringify(cardData1),
+				'second' : JSON.stringify(cardData2),
 			},
-			datatype : "JSON",
-			success : function(obj) { // <= success 부분의 obj 저 부분이 컨트롤러(서버)의 return값이 들어가는 부분이다.
+			datatype : 'JSON',
+			success : function(data) { // <= success 부분의 obj 저 부분이 컨트롤러(서버)의 return값이 들어가는 부분이다.
 
-				if (obj.connect == "success") {
-					for (var i = 0; i < obj.cardList.length; i++) {
+				 if (data.connect == "success") {
+					 // 아랫부분은 굳이 필요가 없었던 for문이였음. 플레이어의 카드는 첫 출력 후 변하지 않음. => 주석처리.
+					 
+					/* for (var i = 0; i < data.cardList.length; i++) {
 						// 객체의 길이를 구하는 방법은 따로있다.
 						// Object.keys(객체의 변수명).length이다.
 
-						list.push((i + 1) + "번째 패 : " + obj.cardList[i].cardNum
+						list.push((i + 1) + "번째 패 : " + data.cardList[i].cardNum
 								+ "<br/>");
-						console.log(obj.cardList[i].cardNum);
+						console.log(data.cardList[i].cardNum);
 						// 배열에 값을 넣어줄때는 배열명.push(넣을 값); 으로 선언한다.
-					}
-
-				}
-				if (obj.connect2 == "success2") {
-					for (var i = 0; i < obj.cardList2.length; i++) {
+					}  */
+					
+					for (var i = 0; i < data.cardList2.length; i++) {
 						/* 위 조건문의 객체의 길이는 객체 전체의 길이다. 나는 카드를 4장을 넣었고 그러므로 객체의 길이는 총 4가 되어버린다.
 						cardlist의 길이를 따로 뽑을순 없을까? 하면서 데브툴즈로 만지작 거리다 obj.cardList.length의 방식으로 선언이
 						가능함을 알았다. ****원래의 오브젝트 배열 길이 뽑는법 : Object.keys(obj).length 이런식.**** */
 						list2.push((i + 1) + "번째 패 : "
-								+ obj.cardList2[i].cardNum + "<br/>");
-						console.log(obj.cardList2[i].cardNum);
+								+ data.cardList2[i].cardNum + "<br/>");
+						console.log(data.cardList2[i].cardNum);
 					}
+
 				}
+				
 				// 엄청난 console.log의 흔적이다. console.log를 생활화 하자. 꼭 
 				// console.log(list);
 				// console.log(list2);
 				// console.log(obj);
 				
 				// console.log(Object.keys(obj).length);
-				firstList.push(list);
-				secondList.push(list2);
-				console.log(firstList);
-				console.log(secondList);
+				/* firstList.push(list);
+				secondList.push(list2); */
 				
-				$("#first").html(list);
+				/* console.log(list); */
+				/* console.log(list2); */
+				
+				/* $("#first").html(list); */
+				
 				$("#second").html(list2);
-				$("#result").html(obj.result);
+				$("#result").html(data.result);
+				document.getElementById("open").style.display="none";
 
 				/*  var firstResult = document.getElementById("first");
 				firstResult.innerHTML = list; */
@@ -122,7 +129,7 @@ thead {
 			},
 			error : function(request, status, error) { // 결과 에러 콜백함수
 				console.log(error);
-			}
+			},
 		});
 	}
 	/* ajax 코드 작성 */
